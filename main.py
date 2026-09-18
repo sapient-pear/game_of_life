2fb9500950e1ff50680c8ee0d2011095eea5f5e5
sapient-pear <daisyevacannon@gmail.com>
2026-09-18 12:02:00 +0100

    added type hints and docstrings

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import curses
 import time
 
 def colour_in_square(display, row, column):
+    """switches whether a cell is alive or dead"""
     if display[row][column] == dead:
         display[row][column] = alive
     else:
         display[row][column] = dead
 
 
-def image_on_screen(stdscr, total_columns, total_rows):
+def image_on_screen(stdscr, total_columns, total_rows) -> list[int, int, curses.window]:
+    """creates window in which game is played"""
     stdscr.clear()
     stdscr.refresh()
     maxy, maxx = stdscr.getmaxyx()
@@ -20,7 +22,8 @@ def image_on_screen(stdscr, total_columns, total_rows):
     return game_window_y, game_window_x, game_window
 
 
-def check_for_new_life(display, row, column):
+def check_for_new_life(display, row, column) -> int:
+    """checks whether a particular cell is alive or dead"""
     if row >= 0 and column >= 0:
         try:
             if display[row][column] == alive:
@@ -34,7 +37,8 @@ def check_for_new_life(display, row, column):
     return count
 
 
-def simulate_game_of_life(display, temp_display):
+def simulate_game_of_life(display, temp_display) -> tuple[list, int]:
+    """updates which cells are alive and which are dead each generation"""
     for row in range(len(display)):
         for column in range(len(display[row])):
             temp_display[row][column] = display[row][column]
@@ -55,7 +59,8 @@ def simulate_game_of_life(display, temp_display):
     return display, different
 
 
-def scanning(display, row, column):
+def scanning(display, row, column) -> int:
+    """checks how many cells are alive around a particular cell"""
     count = 0
     count += check_for_new_life(display, row - 1, column - 1)
     count += check_for_new_life(display, row - 1, column)
@@ -68,14 +73,15 @@ def scanning(display, row, column):
     return count
 
 
-def start_pattern(stdscr, game_window, game_window_x, total_columns):
+def display_title(stdscr, game_window, game_window_x, total_columns):
+    """displays title of game"""
     title = "THE GAME OF LIFE"
     stdscr.move(((stdscr.getmaxyx()[0] - game_window.getmaxyx()[0]) // 4) - 1, (total_columns - len(title)) // 2 + game_window_x)
     typing(title, stdscr)
 
 
 def final_action(stdscr) -> bool:
-    "checks if user wants to play again and returns stop flag"
+    """checks if user wants to play again and returns stop flag"""
     typing("Press p to play again, otherwise press q to quit", stdscr)
     while True:
         action = stdscr.getkey()
@@ -93,11 +99,12 @@ def final_action(stdscr) -> bool:
             return False
 
 
-def play_game(stdscr, total_columns, total_rows):
+def play_game(stdscr, total_columns, total_rows) -> bool:
+    """sets up start pattern and oversees the running of the game"""
     display = [[dead for x in range(total_columns)] for y in range(total_rows)]
     temp_display = [[dead for x in range(total_columns)] for y in range(total_rows)]
     game_window_y, game_window_x, game_window = image_on_screen(stdscr, total_columns, total_rows)
-    start_pattern(stdscr, game_window, game_window_x, total_columns)
+    display_title(stdscr, game_window, game_window_x, total_columns)
     game_window.move(1,1)
     game_window.nodelay(False)
     direction = game_window.getkey()
@@ -141,7 +148,8 @@ def play_game(stdscr, total_columns, total_rows):
 
 
 
-def update_board(display, game_window, total_columns, total_rows, stdscr, gen, game_window_y, game_window_x):
+def update_board(display, game_window, total_columns, total_rows, stdscr, gen) -> int:
+    """displays the updated board each generation"""
     curses.curs_set(0)
     for row in range(total_rows):
         for column in range(total_columns):
@@ -156,13 +164,14 @@ def update_board(display, game_window, total_columns, total_rows, stdscr, gen, g
 
 
 def manual_mode(display, temp_display, stdscr, game_window, total_columns, total_rows, gen, game_window_y, game_window_x):
+    """oversees progressing through the generations when manual mode is selected"""
     char = stdscr.getkey()
     while char != " ":
         if char == "q":
             return
         char = stdscr.getkey()
     display, different = simulate_game_of_life(display, temp_display)
-    gen = update_board(display, game_window, total_columns, total_rows, stdscr, gen, game_window_y, game_window_x)
+    gen = update_board(display, game_window, total_columns, total_rows, stdscr, gen)
     stdscr.addstr(game_window_y + total_rows + 1, game_window_x, f"Generation: {gen}")
     while different != 0:
         char = stdscr.getkey()
@@ -174,16 +183,17 @@ def manual_mode(display, temp_display, stdscr, game_window, total_columns, total
                 break
             char = stdscr.getkey()
         display, different = simulate_game_of_life(display, temp_display)
-        gen = update_board(display, game_window, total_columns, total_rows, stdscr, gen, game_window_y, game_window_x)
+        gen = update_board(display, game_window, total_columns, total_rows, stdscr, gen)
         if different != 0:
             stdscr.addstr(game_window_y + total_rows + 1, game_window_x, f"Generation: {gen}")
 
 
 def auto_mode(display, temp_display, stdscr, game_window, speed, total_columns, total_rows, gen, game_window_y, game_window_x):
+    """oversees progressing through the generations when one of the automatic modes is selected"""
     stdscr.nodelay(True)
     display, different = simulate_game_of_life(display, temp_display)
     curses.napms(speed)
-    gen = update_board(display, game_window, total_columns, total_rows, stdscr, gen, game_window_y, game_window_x)
+    gen = update_board(display, game_window, total_columns, total_rows, stdscr, gen)
     stdscr.addstr(game_window_y + total_rows + 1, game_window_x, f"Generation: {gen}")
     while different != 0:
         char = stdscr.getch()
@@ -192,13 +202,14 @@ def auto_mode(display, temp_display, stdscr, game_window, speed, total_columns, 
         elif char == -1:
             display, different = simulate_game_of_life(display, temp_display)
             curses.napms(speed)
-            gen = update_board(display, game_window, total_columns, total_rows, stdscr, gen, game_window_y, game_window_x)
+            gen = update_board(display, game_window, total_columns, total_rows, stdscr, gen)
             if different != 0:
                 stdscr.addstr(game_window_y + total_rows + 1, game_window_x, f"Generation: {gen}")
     stdscr.nodelay(False)
 
 
-def play_game_impl(display, temp_display, stdscr, game_window_y, game_window_x, game_window, total_columns, total_rows):
+def play_game_impl(display, temp_display, stdscr, game_window_y, game_window_x, game_window, total_columns, total_rows) -> bool:
+    """allows user to select which mode to play the game in"""
     gen = 0
     stdscr.addstr(game_window_y + total_rows + 1, game_window_x, f"Generation: {gen}")
     stdscr.move(game_window_y - 3, game_window_x)
@@ -248,22 +259,24 @@ def play_game_impl(display, temp_display, stdscr, game_window_y, game_window_x, 
 
 
 def typing(sentence, stdscr):
+    """displays a line of text one character after another"""
     col = 0
     curses.curs_set(0)
     for char in range(len(sentence)):
         stdscr.addstr(sentence[char])
         stdscr.refresh()
         col += 1
-        curses.napms(50) # change back to 100 later
+        curses.napms(50)
     curses.curs_set(1)
 
 
 def instructions(stdscr):
+    """displays the instructions for the game"""
     stdscr.addch("\n")
     stdscr.addch("\n")
     typing("WHAT IS THE GAME OF LIFE?\n", stdscr)
     typing("The Game of Life is a simulation invented by mathematician John Conway in which very simple rules cause complex patterns to emerge.\n", stdscr)
-    typing("Each cell in a square grid can either be \"alive\" or \"dead\" (represented in this version by \"*\" and \" \" respectively).\n", stdscr)
+    typing(f"Each cell in a square grid can either be \"alive\" or \"dead\" (represented in this version by {alive} and {dead} respectively).\n", stdscr)
     typing("In order to survive to the next generation, an alive cell must have 2 or 3 neighbouring alive cells.\n", stdscr)
     typing("Each alive cell with 4 or more neighbours will die from overpopulation.\n", stdscr)
     typing("Each alive cell with 1 or 0 neighbours will die from isolation.\n", stdscr)
@@ -291,6 +304,7 @@ def main():
 
 
 def curses_main(stdscr):
+    """introduces the game"""
     while True:
         typing("Welcome to the game of life", stdscr)
         stdscr.addch("\n")
